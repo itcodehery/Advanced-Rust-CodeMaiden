70f4e5721d721d733312a8a4eb58cc8b8a02bc54
Hari Prasad <haririo321@gmail.com>
2025-09-22 16:15:42 +0530

    added LinkedList implementation in Rust

--- dsa_testing/src/main.rs
+++ dsa_testing/src/main.rs
@@ -1,30 +1,51 @@
-use std::io;
+use text_io::read;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Node {
     value: i32,
     next: Option<Box<Node>>,
 }
 
 fn main() {
-    let mut str = String::new();
     println!("Enter an element: ");
-    io::stdin().read_line(&mut str).unwrap();
-    let str = str.trim();
-    let str: i32 = match str.parse() {
-        Ok(res) => res,
-        Err(r) => {
-            println!("Couldn't parse error!");
-            return;
-        }
-    };
-    let head: Node = Node {
-        value: str,
+    let new_int: i32 = read!();
+    let mut head: Box<Node> = Box::new(Node {
+        value: new_int,
         next: None,
-    };
-    for i in 1..5 {
-        if i != 4 {
-            println!("Do you want to add another node?");
+    });
+    let mut current = &mut head;
+    while current.next.is_some() {
+        current = current.next.as_mut().unwrap();
+    }
+    println!("Value of current: {:?}", current);
+    let mut choice: bool = true;
+    while choice != false {
+        // Ask user choice
+        println!("Do you want to add another node?");
+        choice = read!(); // If Choice is true -> add a new Node
+        if choice == true {
+            // ask new value
+            println!("Enter the value: ");
+            let val: i32 = read!();
+            // create new node with next empty
+            let new_node = Node {
+                value: val,
+                next: None,
+            };
+            // if first iteration, set next of head
+            current.next = Some(Box::new(new_node));
+            if current.next.is_some() {
+                current = current.next.as_mut().unwrap();
+            }
         }
     }
+    println!("\nThe Linked List Created: --------------\n");
+    let mut current = &mut head;
+    while current.next.is_some() {
+        print!("{} -> ", current.value);
+        if current.next.is_some() {
+            current = current.next.as_mut().unwrap();
+        }
+    }
+    print!("X");
 }
