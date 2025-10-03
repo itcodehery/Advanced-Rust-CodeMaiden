4cc20c69d8663f3fdfb3cced3b881006969b0c5b
Hari Prasad <haririo321@gmail.com>
2025-10-03 20:58:00 +0530

    added linked list queue implemenattion

--- rust_queue/src/main.rs
+++ rust_queue/src/main.rs
@@ -1,20 +1,52 @@
 // Queue Implementation in Rust
 use text_io::read;
 
-fn enque(queue: [i32; 10], front: i32, rear: i32, element: i32) {}
-
-fn deque(queue: [i32; 10], front: i32, rear: i32) -> i32 {
-    return 0;
+#[derive(Debug, Default)]
+struct Node {
+    val: i32,
+    next: Option<Box<Node>>,
 }
 
-fn display(queue: [i32; 10]) {
-    for i in queue {
-        println!("{}", i);
+impl Node {
+    fn new(val: i32) -> Node {
+        Node {
+            val: val,
+            next: None,
+        }
     }
 }
 
-fn main_loop(queue: &[i32; 10], front: &i32, rear: &i32) {
+fn enqueue(head: &mut Option<Box<Node>>, ele: i32) {
+    let new_node = Box::new(Node::new(ele));
+    let mut current = head;
+    while let Some(node) = current {
+        current = &mut node.next;
+    }
+    *current = Some(new_node);
+}
+
+fn dequeue(head: &mut Option<Box<Node>>) {
+    if let Some(old_head) = head.take() {
+        println!("Dequeued element: {}", old_head.val);
+        *head = old_head.next;
+    } else {
+        println!("Queue is empty.");
+    }
+}
+
+fn display(head: &Option<Box<Node>>) {
+    let mut current = head.as_ref();
+    print!("Queue: ");
+    while let Some(node) = current {
+        print!("{} -> ", node.val);
+        current = node.next.as_ref();
+    }
+    println!("X");
+}
+
+fn main_loop(head: &mut Option<Box<Node>>) {
     let mut ch: i32 = 0;
+
     while ch != -1 {
         println!("-----------");
         println!("Queue Implementation");
@@ -28,13 +60,16 @@ fn main_loop(queue: &[i32; 10], front: &i32, rear: &i32) {
         ch = read!();
         match ch {
             1 => {
-                println!("Enter a number to enque: ");
+                println!("Enter an element to enqueue: ");
                 let ele: i32 = read!();
-                enque(*queue, *front, *rear, ele);
-                println!("The element {} has been enqueued!", ele);
+                enqueue(head, ele);
             }
-            2 => {}
-            3 => {}
+            2 => {
+                dequeue(head);
+            }
+            3 => {
+                display(head);
+            }
             4 => {
                 ch = -1;
             }
@@ -44,9 +79,6 @@ fn main_loop(queue: &[i32; 10], front: &i32, rear: &i32) {
 }
 
 fn main() {
-    let mut queue: [i32; 10] = [-1; 10];
-    let mut front = -1;
-    let mut rear = -1;
-
-    main_loop(&queue, &front, &rear);
+    let mut head: Option<Box<Node>> = None;
+    main_loop(&mut head);
 }
